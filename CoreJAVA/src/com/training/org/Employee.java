09bc10818e3a3d2cf9aeaf18dc05662774389d4f
Mr.Shital B.Madake <sbmadake@gmail.com>
2021-07-12 10:43:01 +0530

    Second change

--- CoreJAVA/src/com/training/org/Employee.java
+++ CoreJAVA/src/com/training/org/Employee.java
@@ -3,5 +3,6 @@ package com.training.org;
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("This is simple message");
+		System.out.println("Another modified message");
 	}
 }
